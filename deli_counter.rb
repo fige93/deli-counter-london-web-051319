6198fbaa61429df7aa37366648141590b917f80b
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-21 10:49:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+
+def line(currentLine)
+  i = 0
+  lineLength = currentLine.length
+  if lineLength == 0
+    puts "The line is currently empty."
+  else
+    lineMessage = "The line is currently:"
+    while i < lineLength
+      lineMessage = "#{lineMessage} #{i+1}. #{currentLine[i]}"
+      i += 1
+    end
+    puts lineMessage
+  end
+end
